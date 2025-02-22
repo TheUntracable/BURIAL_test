51c1fd971eeea78737715a2f54b817b8d6339de8
TheUntracable <julesmontenicolai@gmail.com>
2025-02-22 10:36:00 +0100

    burial.py
    This is the official tool itself. Use this whenever it's ready.

---
+++ burial.py
@@ -0,0 +1,5 @@
+#file is still being made. Do not try to use this as this will do practically noting and errors may occur.
+
+print("Do not use the program yet! We are currently under developement.")
+print("We thank you for your patience.")
+print("Best regards, THE BURIAL PROGRAM//")
